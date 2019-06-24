8e406c6e25b75c3ace90693f53ed19ca5e724120
Billy Learned <BLhoop@live.com>
2019-06-24 08:34:48 -0500

    Adding code to repo

---
+++ BringYourOwnCode/BringYourOwnCode/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BringYourOwnCode.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
